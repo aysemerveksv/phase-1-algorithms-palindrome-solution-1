2c9737720428dd99cc50eb0d2c67aa5ac5cb115e
Ayşe Merve Kosova <mervekosova1@hotmail.com>
2023-04-25 22:24:57 +0300

    completed

--- index.js
+++ index.js
@@ -1,25 +1,60 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  // reverse the input string
+  const reversedWord = reverseString(word);
+  // if the reversed string is the same as the input
+  if (word === reversedWord) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
-/* 
-  Add your pseudocode here
-*/
-
-/*
-  Add written explanation of your solution here
-*/
-
-// You can run `node index.js` to view these console logs
-if (require.main === module) {
-  // add your own custom tests in here
-  console.log("Expecting: true");
-  console.log("=>", isPalindrome("racecar"));
-
-  console.log("");
-
-  console.log("Expecting: false");
-  console.log("=>", isPalindrome("robot"));
+function reverseString(word) {
+  // create an array from the input string
+  const wordArray = word.split("");
+  // reverse the array
+  const reversedWordArray = wordArray.reverse();
+  // create a string from the reversed array
+  const reversedWord = reversedWordArray.join("");
+  // return the reversed string
+  return reversedWord;
 }
 
-module.exports = isPalindrome;
+console.log("Expecting: ih");
+console.log("=>", reverseString("hi"));
+
+console.log("");
+
+console.log("Expecting: tobor");
+console.log("=>", reverseString("robot"));
+
+console.log("");
+
+console.log("Expecting: mom");
+console.log("=>", reverseString("mom"));
+
+console.log("");
+
+// testing the isPalindrome function
+console.log("Expecting: true");
+console.log("=>", isPalindrome("abba"));
+
+console.log("");
+
+console.log("Expecting: true");
+console.log("=>", isPalindrome("racecar"));
+
+console.log("");
+
+console.log("Expecting: true");
+console.log("=>", isPalindrome("a"));
+
+console.log("");
+
+console.log("Expecting: false");
+console.log("=>", isPalindrome("robot"));
+
+console.log("");
+
+console.log("Expecting: false");
+console.log("=>", isPalindrome("ab"));
--- test/indexTest.js
+++ test/indexTest.js
@@ -19,3 +19,32 @@ describe("isPalindrome", () => {
     expect(isPalindrome("ab")).to.be.false;
   });
 });
+if (require.main === module) {
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("racecar"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("mom"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("abba"));
+
+  console.log("");
+
+  console.log("Expecting: true");
+  console.log("=>", isPalindrome("a"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("hi"));
+
+  console.log("");
+
+  console.log("Expecting: false");
+  console.log("=>", isPalindrome("robot"));
+}
